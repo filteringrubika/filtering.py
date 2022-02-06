19923b2c687fdc67bc0b02071345fbbd74b71aff
filteringrubika <99149821+filteringrubika@users.noreply.github.com>
2022-02-07 02:51:02 +0330

    Create filtering.py

---
+++ filtering.py
@@ -0,0 +1,266 @@
+print("Load . . .")                 
+import os
+#What do you have in the source?
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#importing
+import time                                                     
+import sys                                                      
+import colored                                                  
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style                          
+# colors
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+
+
+print ("____________")
+
+os.system("clear")
+
+print(f"{green} ")
+
+print (Fore.WHITE + "")
+print ("Note that this expression is currently old")
+print (" ")
+#date
+x = str(datetime.datetime.now())
+
+print("Beginning " + (x))
+
+print(f"{blue} ")
+print ("         GOD FILTERING")
+print ("   They are the only creators of (Ryson) Mammad")
+print ("""The best filtering rule in virtual: (law-ryson.blogfa.com There are Ryson! rules.""")
+print (Fore.YELLOW + "")
+print (""" Supermarket code reporter in virtual by AmiR""")
+print (Fore.GREEN + "")
+x = """ Loading ~ ~ ~ 
+╠╬╬╬╬╬╬╬╬╬╬╬╬╣🖤❤ 
+❤╠╬╬╬████╬╬╬╬╬╣❤🖤 
+🖤╠╬╬╬╬██╬╬╬╬╬╬╣🖤❤ 
+❤╠╬╬╬╬██╬╬╬╬╬╬╣❤🖤 
+🖤╠╬╬╬╬██╬╬╬╬╬╬╣🖤❤ 
+❤╠╬╬╬╬██╬╬█╬╬╬╣❤🖤 
+🖤╠╬╬╬██████╬╬╬╣🖤❤ 
+❤╠╬╬╬╬╬╬╬╬╬╬╬╬╣❤🖤 
+🖤╠╬╬╬╬╬╬╬╬╬╬╬╬╣🖤❤ 
+❤╠╬╬╬╬████╬╬╬╬╣❤🖤 
+🖤╠╬╬╬█╬╬╬╬█╬╬╬╣🖤❤ 
+❤╠╬╬██╬╬╬╬██╬╬╣❤🖤 
+🖤╠╬╬██╬╬╬╬██╬╬╣🖤❤ 
+❤╠╬╬╬█╬╬╬╬█╬╬╬╣❤🖤 
+🖤╠╬╬╬╬████╬╬╬╬╣🖤❤ 
+❤╠╬╬╬╬╬╬╬╬╬╬╣❤🖤 
+🖤╠╬╬╬╬╬╬╬╬╬╬╬╬╣🖤❤ 
+❤╠╬███╬╬╬╬███╬╣❤🖤 
+🖤╠╬╬██╬╬╬╬██╬╬╣🖤❤ 
+❤╠╬╬╬█╬╬╬╬█╬╬╬╣❤🖤 
+🖤╠╬╬╬╬█╬╬█╬╬╬╬╣🖤❤ 
+❤╠╬╬╬╬╬██╬╬╬╬╬╣❤🖤 
+🖤╠╬╬╬╬╬╬╬╬╬╬╬╬╣🖤❤ 
+❤╠╬╬╬╬╬╬╬╬╬╬╬╬╣❤🖤 
+🖤╠╬╬████████╬╬╣🖤❤ 
+❤╠╬╬╬██╬╬╬╬█╬╬╣❤🖤 
+🖤╠╬╬╬██╬█╬╬╬╬╬╣🖤❤ 
+❤╠╬╬█████╬╬╬╬╬╣❤🖤 
+🖤╠╬╬╬██╬█╬╬╬╬╬╣🖤❤ 
+❤╠╬╬╬██╬╬╬╬█╬╬╣❤🖤 
+🖤╠╬╬████████╬╬╣🖤❤ 
+❤╠╬͍╬╬͍╬╬͍╬╬͍╬╬͍╬╬͍╬╣͍❤🖤 
+🖤╠͍╬╬͍╬╬͍╬╬͍╬╬͍╬╬͍╬╬͍╣🖤❤ 
+💙┼┼▄██▄┼♥┼▄██▄┼┼💙 
+🖤┼██████💗██████┼🖤 
+❤┼██████💜██████┼❤ 
+🖤┼┼█████💚█████┼┼🖤 
+❤┼┼┼████💙████┼┼┼❤ 
+🖤┼┼┼┼┼██❤██┼┼┼┼┼🖤 
+❤┼┼┼┼┼┼█💛█┼┼┼┼┼┼❤ 
+🖤❤🖤 ❤ 🖤❤🖤❤🖤 ❤   """
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.01)
+
+print(f"{red} ")
+d = """
+I Lobe You-
+Manufacturer- _____
+                            (Loding....)
+         GOD FILTERING RUBIKA kancifer
+/__________ /-/-/
+                    CODE FILTERI RUBIKA
+             __________ 💗
+     WEBLOG GHAVANIN ~> ____.Com
+          
+          Search Google "______" Please!
+FILTER RUBIKA ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||_ _ _ _ _
+"""
+for d in d:
+        sys.stdout.write(d)
+        sys.stdout.flush()
+        time.sleep(0.03)
+print("Loding....")
+import os
+#What do you have in the source?
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#importing
+import time                                                     
+import sys                                                      
+import colored                                                  
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style                          
+# colors
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+
+os.system("clear")
+
+print(f"{green} ")
+
+print (Fore.WHITE + "")
+print ("Note that this expression is currently old")
+print (" ")
+#date
+x = str(datetime.datetime.now())
+
+print("Beginning " + (x))
+
+print(f"{blue} ")
+print ("         GOD FILTERING")
+print ("   They are the only creators of (_____) ______")
+print ("""The best filtering rule in virtual: (law-ryson.blogfa.com There are Ryson! rules.""")
+print (Fore.YELLOW + "")
+print (""" Supermarket code reporter in virtual by ___ """)
+print (Fore.GREEN + "")
+x = """ Loading ~ ~ ~         """
+print("_____")
+print ("")
+print (" ")
+print ("")
+# 1 chanel 2 Group 3 acc
+up = input(""" 
+[kode   (1) ]
+[kode1     (2) ]
+                 
+[kode2  (3) ]
+                  
+[kode 3    (0) ]
+[Exit     (4) ]
+[kode 5  (5) ]
+[kode 6  (6) ]
+Which do you want>>> """)
+print("")
+print(f"{pink} ")
+time.sleep(3.0)
+print(""" in the name of God 
+_____________
+ _   _
+  ________//_______
+    
+||Web: creator-______.blogfa.com||
+|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>||
+________rubika.ir/Caetorr________
+""")
+print(f"{red} ")
+
+if up == "0":
+         print ('(Super) Code {filter} =>' + '%s%s (4.0.4.5/y//d/f/4.0.8.9.4.0.1.5.7.8.0.1.3.0.7.3.6.1///filtering.6.2.1.6(8.9.3)1.5.6.0.1.3) %s' % (fg('red'), bg('black'), attr('reset')))
+
+
+if up == "1":
+
+        print ('code {Filter} (channel) ≠>' + '%s%s (7.0.6.4/f//d.5.2.7.9.0.9.1.3.5.0.3.1.7.8.9.2.6.0.3.8.4.3.07.3.6.1///filtering.6.2.1.6(8.9.3).2) %s' % (fg('red'), bg('green'), attr('reset')))
+
+
+if up != "1":
+        print("    COD  ")
+
+
+if up == "2":
+        print ('code {Filter} (group) ≠>' + '%s%s (9.0.3.7/f////g.h/5.2.7.9.5.0.1.2.6.0.8.1.7.0.7.3.3.7.3.6.1///filtering.6.2.1.6(8.9.3)8.6.4) %s' % (fg('white'), bg('yellow'), attr('reset')))
+
+
+if up != "2":
+        print("     cσ∂s мү ______ ")
+
+
+if up == "3":
+
+        print ('code {Filter} (user) ≠>' + '%s%s (4.0.7.5/f//a/y/7.0.9.3.6.0.3.2.5.3.6.8.5.0.4.9.4.0.7.3.6.1///filtering.6.2.1.6(8.9.3)3.6.09/) %s' % (fg('red'), bg('blue'), attr('reset')))
+
+
+if up != "3":
+        print("""_______""")
+             
+             
+if up == "4":
+
+        print ('code {Filter} ≠>' + '%s%s (7.0.6.4/f//d.5.2.7.9.0.9.1.3.5.0.3.1.7.8.9.2.6.0.3.8.4.3.7.3.6.1///filtering.6.2.1.6(8.9.3)0.2) %s' % (fg('red'), bg('green'), attr('reset')))
+
+
+if up != "4":
+        print("    (################)      ")
+
+
+if up == "5":
+        print ('code {Filter} ≠>' + '%s%s (9.0.3.7/f////g.h/5.2.7.9.5.0.1.2.6.0.8.1.7.0.7.3.3.8.7.3.6.1///filtering.6.2.1.6(8.9.3)6.4) %s' % (fg('white'), bg('yellow'), attr('reset')))
+
+
+if up != "5":
+        print("    ________")
+
+
+if up == "6":
+
+        print ('code {Filter} ≠>' + '%s%s (4.0.7.5/f//a/y/7.0.9.3.6.0.3.2.5.3.6.8.5.0.4.9.4.0.3.7.3.6.1///filtering.6.2.1.6(8.9.3)6.09/) %s' % (fg('red'), bg('blue'), attr('reset')))
+
+
+if up != "6":
+        print("""
+        
+             (#########)     """)
+
+time.sleep(0.6)
+print(f"{green}" )
+print ("""
+Kanal rubika __________=
+@______
+ """)
+print ("______")
+print (" ________") 
+print(f"{blue} ")
+time.sleep(0.6)
+print ("Loding...")
+time.sleep(0.2)
+print ("(######)")
+time.sleep(1.0)
+print ("(#####)")
+time.sleep(0.6)
+print ("(####)")
+time.sleep(0.2)
+print ("(###)")
+time.sleep(0.6)
+print ("(##)")
+time.sleep(0.4)
+print ("(#)")
+time.sleep(0.4)
+print ("(##)")
+time.sleep(0.3)
+print ("(###)")
+time.sleep(0.2)
+print ("(####)")
+print ("   godbay    ")
